4d3aa37ad9c7ebe565f9a3dc9d8b5db463e9c3ba
irina.pauna0311 <irina.pauna0311@stud.acs.upb.ro>
2026-03-12 22:27:54 +0200

    start project 2

--- social/twitter/admin.py
+++ social/twitter/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inline = [ProfileInline]
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
+    
